c1b7651163b9eea60fddfafa134bfb52aa9954bc
Marius-Constantin Dinu <dinu.marius-constantin@hotmail.com>
2016-12-26 21:09:31 +0100

    Added player movement

---
+++ BallDemo/Assets/Custom/Scripts/CameraPlayerRotation.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CameraPlayerRotation : MonoBehaviour
+{
+    public Transform lookAt;
+
+    private Vector3 desiredPosition;
+    private Vector3 offset;
+
+    private float smoothSpeed = 7.5f;
+    private float distance = 3f;
+    private float yOffset = 3.5f;
+
+    void Start()
+    {
+        offset = new Vector3(0, yOffset - 1, -2f*distance);
+    }
+    
+    void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonUp("TurnLeft"))
+        {
+            SlideCamera(0);
+        }
+        else if (CrossPlatformInputManager.GetButtonUp("TurnRight"))
+        {
+            SlideCamera(1);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        desiredPosition = lookAt.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.LookAt(lookAt.position);
+    }
+
+    void SlideCamera(int direction)
+    {
+        switch (direction)
+        {
+            case 0:
+                offset = Quaternion.Euler(0, -90, 0) * offset;
+                break;
+            case 1:
+                offset = Quaternion.Euler(0, 90, 0) * offset;
+                break;
+        }
+    }
+
+}
